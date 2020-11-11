1678a88ae6bdd160de35dc4be344efcbcaae9479
Alisa Subbotina <alisasbbtn@gmail.com>
2020-11-11 22:02:59 +0200

    Improve TaskInout stylibg

--- src/components/TaskInput/TaskInput.js
+++ src/components/TaskInput/TaskInput.js
@@ -6,7 +6,7 @@ const taskInput = () => {
   return (
     <div className={classes.TaskInput}>
       <input className={classes.Input} type="text" placeholder="Add a new task..."></input>
-      <button className={classes.Button}><ion-icon name="add-circle-outline" size="large" title="Add Task"></ion-icon></button>
+      <button className={classes.Button}><ion-icon name="add-circle" title="Add Task"></ion-icon></button>
     </div>
   );
 };
